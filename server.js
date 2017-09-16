ca28c6acfaea7f64a2a86ee2af6a762ee6f16568
Hack Reactor Student <student@hackreactor.com>
2017-09-15 19:20:18 -0700

    listening to ports

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
 var port = 4568;
-
-app.listen(port);
-
-console.log('Server now listening on port ' + port);
+app.set('port', (process.env.PORT || port));
+app.listen(app.get('port'), function() {
+    console.log('Server now listening on port ', app.get('port'));
+});
